158871abdd2f9050bffecf5a7468ea272b356ab0
1gamseong <rkatjddlsp@gmail.com>
2025-03-31 18:16:22 +0900

    이미지추가,코레아파일추가
    이미지추가,코레아파일추가

--- food_4t_app/lib/korea/korea_p1.dart
+++ food_4t_app/lib/korea/korea_p1.dart
@@ -8,8 +8,79 @@ class KoreaP1 extends StatefulWidget {
 }
 
 class _KoreaP1State extends State<KoreaP1> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+
+  
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(backgroundColor: const Color.fromARGB(255, 55, 76, 170),
+      title: Row(
+        children: [
+          Image.asset('images/korea.png', height: 40, width: 60),
+          SizedBox(width: 6),
+          Padding(
+            padding: const EdgeInsets.all(300.0),
+            child: Text("한식",style: TextStyle(color: const Color.fromARGB(255, 240, 18, 2),),))
+        ],
+      ),
+    ),
+body: Center(
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        "한국의 음식",
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+      SizedBox(height: 16),
+      Row(children: [
+              CircleAvatar(radius: 100,
+        backgroundImage:AssetImage('images/korea.png')
+        )
+
+      ],
+      ),
+      Row(
+        children: [
+          Icon(Icons.circle, size: 6),
+          SizedBox(width: 6),
+          Text("한국 음식은 발효와 국물 요리가 발달해 깊은 맛을 자랑한다."),
+        ],
+      ),
+      Row(
+        children: [
+          Icon(Icons.circle, size: 6),
+          SizedBox(width: 6),
+          Text("김치찌개는 김치와 돼지고기를 넣고 끓인 얼큰한 찌개요리."),
+        ],
+      ),
+      Row(
+        children: [
+          Icon(Icons.circle, size: 6),
+          SizedBox(width: 6),
+          Text("삼계탕은 인삼과 찹쌀을 넣은 닭 요리로 여름 보양식."),
+        ],
+      ),
+      Row(
+        children: [
+          Icon(Icons.circle, size: 6),
+          SizedBox(width: 6),
+          Text("건강과 계절을 고려한 조화로운 식문화가 특징입니다."),
+        ],
+      ),
+              Row(
+          children: [
+            ElevatedButton(onPressed: () => Navigator.popAndPushNamed(context,'/' ),
+                 child: Text('메인')),
+            ElevatedButton(onPressed: () => Navigator.popAndPushNamed(context,'/k2' ),
+          child: Text('다음')),
+          ],
+        ),
+
+              ],
+  ),
+                  ),
+  );
+}
 }
--- food_4t_app/lib/korea/korea_p3.dart
+++ food_4t_app/lib/korea/korea_p3.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class KoreaP3 extends StatefulWidget {
@@ -8,8 +10,107 @@ class KoreaP3 extends StatefulWidget {
 }
 
 class _KoreaP3State extends State<KoreaP3> {
+  late List<String> image1;
+  late int current;
+  late Timer _timer;
+
+  @override
+  void initState() {
+
+    super.initState();{
+      image1 = [
+        'chicken.png',
+        'chicken2.png',
+      ];
+      current = 0;
+      _timer = Timer.periodic(Duration(seconds: 3), (timer) {
+        change();
+      });
+    }
+  }
+
+  change(){
+    current++;
+    if(current>=image1.length){
+      current=0;
+    }
+    if (mounted) {
+      setState(() {
+        
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+    appBar: AppBar(backgroundColor: const Color.fromARGB(255, 64, 82, 160),
+      title: Row(
+        children: [
+          Image.asset('images/korea.png', height: 40, width: 60),
+          SizedBox(width: 6),
+          Padding(
+            padding: const EdgeInsets.all(300.0)
+            ),Text("한식",style: TextStyle(color: const Color.fromARGB(255, 240, 18, 2)),),
+          ],
+        ),
+      ),
+      body: Column(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [
+    Text(
+      "삼계탕",
+      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    ),
+    SizedBox(height: 16),
+    Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          radius: 100,
+          backgroundImage: AssetImage('images/${image1[current]}'),
+        ),
+      ],
+    ),
+    SizedBox(height: 16),
+    Row(
+      children: [
+        Icon(Icons.circle, size: 6),
+        SizedBox(width: 6),
+        Expanded(child: Text("삼계탕은 영계(어린 닭) 안에 인삼, 찹쌀, 대추 등을 넣고 푹 끓인 보양식이야.")),
+      ],
+    ),
+    Row(
+      children: [
+        Icon(Icons.circle, size: 6),
+        SizedBox(width: 6),
+        Expanded(child: Text("여름철 더위에 기력을 보충하기 위해 초복, 중복, 말복에 자주 먹어.")),
+      ],
+    ),
+    Row(
+      children: [
+        Icon(Icons.circle, size: 6),
+        SizedBox(width: 6),
+        Expanded(child: Text("한입 먹으면 기분 좋아지는 존맛탱.")),
+      ],
+    ),
+        Row(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () => Navigator.popAndPushNamed(context,'/k2' ),
+                 child: Text('이전')),
+            ElevatedButton(onPressed: () => Navigator.popAndPushNamed(context,'/' ),
+          child: Text('다음')),
+          ],
+        ),
+
+  ],
+),
+      );
+      }
   }
-}
