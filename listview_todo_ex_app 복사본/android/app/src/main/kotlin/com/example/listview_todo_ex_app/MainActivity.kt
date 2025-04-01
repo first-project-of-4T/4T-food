dbaaea8f34b5b5fab26d772a6cb056a3ecded363
smitepaladin <darktemper20701@gmail.com>
2025-04-01 17:25:06 +0900

    Delete listview_todo_ex_app 복사본 directory
    삭제

--- listview_todo_ex_app 복사본/android/app/src/main/kotlin/com/example/listview_todo_ex_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.listview_todo_ex_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- listview_todo_ex_app 복사본/lib/model/message.dart
+++
@@ -1,9 +0,0 @@
-class Message {
-  //Property 생성자가 없다 프로퍼티만 있다
-  static String workList = ""; // 데이터를 계속 가지고 있는 프로퍼티
-  static String imagePath = "";
-  static bool action = false; // 입력하는 애가 준 것이 true인지 아닌지 판단기준 변수, false일때는 저장되지 않게 거른다.
-  static bool switchBuy = true;
-  static bool switchPromise = false;
-  static bool switchStudy = false;
-}
